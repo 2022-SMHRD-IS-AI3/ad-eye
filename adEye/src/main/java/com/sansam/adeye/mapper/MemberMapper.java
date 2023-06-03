39864322afdbaaf60fcb5c2366ec5137fd9beb01
smhrd <smhrd@220.93.229.245>
2023-06-03 15:53:03 +0900

    MemberMapper 내용 추가

--- adEye/src/main/java/com/sansam/adeye/mapper/MemberMapper.java
+++ adEye/src/main/java/com/sansam/adeye/mapper/MemberMapper.java
@@ -1,5 +1,12 @@
 package com.sansam.adeye.mapper;
 
+import com.sansam.adeye.domain.MemberDTO;
+
 public interface MemberMapper {
+	
+	public void insert(MemberDTO mbtn);
+	public void delete(String id);
+	public void update(MemberDTO mbtn);
+	public void login(MemberDTO mbtn);
 
 }
