2d78ea6b7b0a7b7974f412905e815d47e18eb6cc
smhrd <smhrd@220.93.229.245>
2023-06-03 16:47:14 +0900

    ?

--- adEye/src/main/java/com/sansam/adeye/mapper/MemberMapper.java
+++ adEye/src/main/java/com/sansam/adeye/mapper/MemberMapper.java
@@ -1,12 +1,16 @@
 package com.sansam.adeye.mapper;
 
+import java.util.List;
+
 import com.sansam.adeye.domain.MemberDTO;
 
 public interface MemberMapper {
 	
 	public void insert(MemberDTO mbtn);
-	public void delete(String id);
-	public void update(MemberDTO mbtn);
+	public int delete(String id);
+	public int update(MemberDTO mbtn);
 	public void login(MemberDTO mbtn);
+	public MemberDTO detail(String mem_id);
+	public List<MemberDTO> selectall();
 
 }
