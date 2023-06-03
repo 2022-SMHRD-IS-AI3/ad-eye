59e0178471e6638f20da1ba2222243cadaf36b7b
smhrd <smhrd@218.157.24.2>
2023-06-03 17:16:11 +0900

    왜 멤버만 안되니

--- adEye/src/main/java/com/sansam/adeye/persistence/IDeviceDAO.java
+++ adEye/src/main/java/com/sansam/adeye/persistence/IDeviceDAO.java
@@ -1,5 +1,24 @@
 package com.sansam.adeye.persistence;
 
+import java.util.List;
+
+import com.sansam.adeye.domain.Criteria;
+import com.sansam.adeye.domain.DeviceDTO;
+
 public interface IDeviceDAO {
 
+	// 전체 기기 조회
+	public List<DeviceDTO> deviceList(Criteria cri) throws Exception;
+	// 기기 생성
+	public void deviceInsert(DeviceDTO dDto) throws Exception;
+	// 특정 기기 수정
+	public int deviceUpdate(DeviceDTO dDto) throws Exception;
+	// 특정 기기 삭제 (상태 변경)
+	public int deviceDelete(DeviceDTO dDto) throws Exception;
+	// 특정 기기 정보 조회
+	public DeviceDTO deviceDetail(Integer seq) throws Exception;
+	// 특정 기기 컨트롤
+	public int deviceControl(DeviceDTO dDto) throws Exception;
+	//특정 기기 로그 조회
+	public List<DeviceDTO> deviceLog(Criteria cri) throws Exception;
 }
--- adEye/src/main/java/com/sansam/adeye/persistence/impl/DeviceDAO.java
+++ adEye/src/main/java/com/sansam/adeye/persistence/impl/DeviceDAO.java
@@ -1,7 +1,53 @@
 package com.sansam.adeye.persistence.impl;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sansam.adeye.domain.Criteria;
+import com.sansam.adeye.domain.DeviceDTO;
 import com.sansam.adeye.persistence.IDeviceDAO;
 
+@Repository
 public class DeviceDAO implements IDeviceDAO{
+	
+	@Autowired
+	private SqlSession session;
 
+	// 전체 기기 조회 : /
+	@Override
+	public List<DeviceDTO> deviceList(Criteria cri) throws Exception {
+		return session.selectList("DeviceMapper.list", cri);
+	}
+	// 기기 생성 : /insert
+	@Override
+	public void deviceInsert(DeviceDTO dDto) throws Exception {
+		session.insert("DeviceMapper.create", dDto);	
+	}
+	// 특정 기기 조회 : /detail
+	public DeviceDTO deviceDetail(Integer seq) throws Exception {
+		return session.selectOne("DeviceMapper.detail", seq);
+	}	
+	// 특정 기기 수정 : /update
+	@Override
+	public int deviceUpdate(DeviceDTO dDto) throws Exception {
+		return session.update("DeviceMapper.update", dDto);
+	}
+	// 특정 기기 삭제 (상태 변경) : /delete
+	@Override
+	public int deviceDelete(DeviceDTO dDto) throws Exception {
+		return session.update("DeviceMapper.delUpdate", dDto);
+	}
+	// 특정 기기 컨트롤 : /control
+	@Override
+	public int deviceControl(DeviceDTO dDto) throws Exception {
+		return session.update("DeviceMapper.control", dDto);
+	}		
+	// 특정 기기 로그 조회 : /log
+	@Override
+	public List<DeviceDTO> deviceLog(Criteria cri) throws Exception {
+		return session.selectList("DeviceMapper.매퍼ID", cri);
+	}
 }
