6b7d69332ae200678df27ca164ec3d5fd0b214dc
smhrd <smhrd@220.93.229.245>
2023-06-03 10:01:22 +0900

    commit용

--- adEye/src/main/java/com/sansam/adeye/domain/AcquisitionDTO.java
+++ adEye/src/main/java/com/sansam/adeye/domain/AcquisitionDTO.java
@@ -1,5 +1,7 @@
 package com.sansam.adeye.domain;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class AcquisitionDTO {
 
 	private int acq_seq;
+	
 }
