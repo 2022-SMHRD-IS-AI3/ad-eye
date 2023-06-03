592ffacdb0127728df3a11bb6eaf779a78c88c98
SMHRD <SMHRD@211.223.37.186>
2023-06-03 10:09:09 +0900

    주연커밋

---
+++ adEye/src/main/java/com/sansam/adeye/AcquisitionController.java
@@ -0,0 +1,41 @@
+package com.sansam.adeye;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sansam.adeye.domain.AcquisitionSubmitDTO;
+import com.sansam.adeye.domain.MemberDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/acquisition")
+@Log4j
+public class AcquisitionController {
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public @ResponseBody Map<String,Object> member(AcquisitionSubmitDTO data) { // @ResponseBody : 응답할 때 JSON 데이터로 반환
+		log.info("/member.................");
+		
+		
+		
+		// 보내줄 맵 객체 생성,
+	    Map<String,Object> paramMap = new HashMap<String, Object>();
+
+	    // paramMap 담을 객체 생성
+	    Map<String,Object> paramMap2 = new HashMap<String, Object>();
+	      
+	    paramMap2.put("mem-id", "ad230531xxx");
+	    paramMap.put("result", paramMap2);
+	    paramMap.put("code", "201");
+	    paramMap.put("message", "등록 성공");
+	    
+	    // {"result":{"mem-id":"ad230531xxx"},"code":"201","message":"등록 성공"}
+		return paramMap;
+		
+	}
+}
