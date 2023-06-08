87adef497095feede4811f0eb8ae289f2dcc9685
smhrd <smhrd@59.0.147.229>
2023-06-08 09:28:08 +0900

    ContactDTO 내용 추가

--- adEye/src/main/java/com/sansam/adeye/domain/ContactDTO.java
+++ adEye/src/main/java/com/sansam/adeye/domain/ContactDTO.java
@@ -1,5 +1,7 @@
 package com.sansam.adeye.domain;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +12,12 @@ import lombok.NoArgsConstructor;
 public class ContactDTO {
 
 	private int contact_seq;
+	private String email;
+	private String phone;
+	private int company;
+	private int contact_content;
+	private Date contact_dt;
+	private int contact_open;
+	private int admin_id;
+	
 }
