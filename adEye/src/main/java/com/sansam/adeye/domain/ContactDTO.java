30ee7679f300c00f3258e8e4379df209e267d721
smhrd <smhrd@218.157.24.2>
2023-06-09 16:47:17 +0900

    Contact DTO / DAO / Service / Controller / Mapper DB통신확인까지 완료

--- adEye/src/main/java/com/sansam/adeye/domain/ContactDTO.java
+++ adEye/src/main/java/com/sansam/adeye/domain/ContactDTO.java
@@ -23,28 +23,22 @@ public class ContactDTO {
 	private String company;
 	@NonNull
 	private String contact_content;
-	private String contact_dt;
+	private Date contact_dt;
 	@NonNull
-	private String contact_open;
+	private char contact_open;
 	private String admin_id;
 	
 	// 전체 문의 조회 용 생성자
-//	public ContactDTO(int contact_seq, @NonNull String email, @NonNull String phone, 
-//			@NonNull String company, @NonNull String contact_content, @NonNull Date contact_dt,
-//			@NonNull char contact_open) {
-//		this.contact_seq = contact_seq;
-//		this.email = email;
-//		this.phone = phone;
-//		this.company = company;
-//		this.contact_content = contact_content;
-//		this.contact_dt = contact_dt;
-//		this.contact_open = contact_open;		
-//	}
-	
-	// 문의 등록 용 생성자
-	
-	
-	// 문의 상세 조회 용 생성자
-	
+	public ContactDTO(int contact_seq, @NonNull String email, @NonNull String phone, 
+			@NonNull String company, @NonNull String contact_content, Date contact_dt,
+			@NonNull char contact_open) {
+		this.contact_seq = contact_seq;
+		this.email = email;
+		this.phone = phone;
+		this.company = company;
+		this.contact_content = contact_content;
+		this.contact_dt = contact_dt;
+		this.contact_open = contact_open;		
+	}
 	
 }
