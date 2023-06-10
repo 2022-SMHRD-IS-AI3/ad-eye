ac69559e1d31cfe2f72b6fb4c9efed8b46300299
SMHRD <SMHRD@211.223.37.186>
2023-06-10 18:06:38 +0900

    주연 프론트 user_dashboard chart-pie-demo

--- adEye/src/main/webapp/resources/assets/demo/chart-pie-demo.js
+++ adEye/src/main/webapp/resources/assets/demo/chart-pie-demo.js
@@ -3,27 +3,27 @@
 '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = "#858796";
 
+
+const data = {
+    labels: [
+      'Female',
+      'Male'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [25411, 43348],
+      backgroundColor: [
+        'rgb(255, 128, 122)',
+        'rgb(54, 162, 235)'
+      ],
+      hoverOffset: 4
+    }]
+  };
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
-    type: "doughnut",
-    data: {
-        labels: ["Direct", "Referral", "Social"],
-        datasets: [{
-            data: [55, 30, 15],
-            backgroundColor: [
-                "rgba(0, 97, 242, 1)",
-                "rgba(0, 172, 105, 1)",
-                "rgba(88, 0, 232, 1)"
-            ],
-            hoverBackgroundColor: [
-                "rgba(0, 97, 242, 0.9)",
-                "rgba(0, 172, 105, 0.9)",
-                "rgba(88, 0, 232, 0.9)"
-            ],
-            hoverBorderColor: "rgba(234, 236, 244, 1)"
-        }]
-    },
+    type: "pie",
+    data: data,
     options: {
         maintainAspectRatio: false,
         tooltips: {
@@ -31,14 +31,11 @@ var myPieChart = new Chart(ctx, {
             bodyFontColor: "#858796",
             borderColor: "#dddfeb",
             borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
             displayColors: false,
-            caretPadding: 10
+            caretPadding: 0
         },
         legend: {
             display: false
         },
-        cutoutPercentage: 80
     }
 });
